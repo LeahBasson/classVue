65b1db3e3310366642dbc6aae2faaf3e899516e5
LeahBasson <leahbasson54@gmail.com>
2024-06-24 10:02:54 +0200

    Class Example

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <ClassExample/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ClassExample from '@/components/ClassExample.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ClassExample
   }
 }
 </script>
---
+++ src/components/ClassExample.vue
@@ -0,0 +1,27 @@
+<template>
+  <div :class="{ active: isActive, 'text-danger': hasError }">
+    Dynamic Class Binding
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isActive: true,
+      hasError: false,
+    };
+  },
+};
+</script>
+
+<style>
+.active {
+  background-color: green;
+  color: white;
+}
+
+.text-danger {
+  color: red;
+}
+</style>
